d5534232159faee1316bccb1480f42d82ee24a5e
abdelrhmanelgeny <abdelrhmanelgendy243@gmail.com>
2023-06-17 10:46:20 +0800

    -8

--- app/src/main/java/com/tasks/worker_manager/WorkerActivity.kt
+++ app/src/main/java/com/tasks/worker_manager/WorkerActivity.kt
@@ -66,26 +66,26 @@ class WorkerActivity : ComponentActivity() {
 
 //             Log.d("TAG", "startSimpleWorker: 001 $hasPhone ")
 
-             if (hasPhone=="1") {
-                // You know it has a number so now query it like this
-                val phones = contentResolver
-                    .query( ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                        null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+
-                                contactId, null, null);
-                while (phones!!.moveToNext()) {
-                    val phoneNumber = phones.getString(phones.getColumnIndex( ContactsContract.CommonDataKinds.Phone.PHO));
-                    val name = phones.getString(phones.getColumnIndex( ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-
-
-
-                    if((phoneNumber?:"").contains("182"))
-                    {
-                        Log.d("TAG", "startSimpleWorker: $name    "+phoneNumber)
-
-                    }
-                }
-                phones.close();
-            }
+//             if (hasPhone=="1") {
+//                // You know it has a number so now query it like this
+//                val phones = contentResolver
+//                    .query( ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+//                        null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+
+//                                contactId, null, null);
+//                while (phones!!.moveToNext()) {
+//                    val phoneNumber = phones.getString(phones.getColumnIndex( ContactsContract.CommonDataKinds.Phone.PHO));
+//                    val name = phones.getString(phones.getColumnIndex( ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+//
+//
+//
+//                    if((phoneNumber?:"").contains("182"))
+//                    {
+//                        Log.d("TAG", "startSimpleWorker: $name    "+phoneNumber)
+//
+//                    }
+//                }
+//                phones.close();
+//            }
 
 //            val emails = getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
 //                null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + contactId, null, null);
