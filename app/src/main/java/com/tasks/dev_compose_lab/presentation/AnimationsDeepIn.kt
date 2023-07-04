1de786cb1319cfeba97352728deb42a29edc0be2
Abdelrhman <abdelrhmanelgendy243@gmail.com>
2023-07-04 16:25:25 +0300

    animation cont,

--- app/src/main/java/com/tasks/dev_compose_lab/presentation/AnimationsDeepIn.kt
+++ app/src/main/java/com/tasks/dev_compose_lab/presentation/AnimationsDeepIn.kt
@@ -3,17 +3,17 @@ package com.tasks.dev_compose_lab.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.tasks.navigationcomponent.ui.theme.NavigationComponentTheme
@@ -28,6 +28,8 @@ class AnimationsDeepIn : ComponentActivity() {
             }
         }
     }
+
+    var count = 0
 
     @Composable
     fun Content() {
@@ -47,30 +49,79 @@ class AnimationsDeepIn : ComponentActivity() {
 //                repeatMode = RepeatMode.Reverse
 //            )
 //        )
-        val animateInt by finiteTransition.animateInt() { rounded ->
-            if (rounded) 200 else 50
+        val valueAnimation by animateFloatAsState(targetValue =if(isRounded) 150f else 0f,
+            animationSpec = spring(dampingRatio = Spring.DampingRatioHighBouncy, stiffness = Spring.StiffnessHigh)
+        )
 
-        }
 
+        val rotation by animateFloatAsState(
+            if (isRounded) 360f else 0f,
+            animationSpec = tween(durationMillis = 1000),
+            finishedListener = {
+                if(count<1)
+                {
+                    isRounded = !isRounded
+                    count++
+                }
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize(
-            )
-        ) {
-            Box(modifier = Modifier.height(200.dp))
-            Button(onClick = {
-                isRounded = !isRounded
-            }) {
-                Text(text = "Toggle")
             }
+        )
+        val coloration by animateColorAsState(
+            if (isRounded) Color.White else Color.DarkGray,
+            animationSpec = tween(durationMillis = 1000),
+            finishedListener = {
+                if(count<1)
+                {
+                    isRounded = !isRounded
+                    count++
+                }
+
+            }
+        )
+
+        val colorationBox by animateColorAsState(
+            if (isRounded) Color.DarkGray else Color.White,
+            animationSpec = tween(durationMillis = 1000),
+            finishedListener = {
+                if(count<1)
+                {
+                    isRounded = !isRounded
+                    count++
+
+                    throw RuntimeException("Test Crash") // Force a cras
+                }
+
+            }
+        )
 
 
-            Box(
-                modifier = Modifier
-                    .size(animateInt.dp)
-                     .background(Color.Red)
 
-            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize().background(colorationBox)
+            ) {
+                Box(modifier = Modifier.height(200.dp))
+                Button(onClick = {
+                    count = 0
+
+                    isRounded = !isRounded
+
+                }) {
+                    Text(text = "Toggle")
+                }
+
+
+                Box(
+                    modifier = Modifier
+                        //                    .padding(top = (valueAnimation.coerceAtLeast(0f)).dp)
+                        .size(50.dp)
+                        .rotate(rotation)
+
+                        .background(coloration)
+
+                )
+
 
 
         }
